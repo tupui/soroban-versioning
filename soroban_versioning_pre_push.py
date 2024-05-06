7e03f43d7cb1bb01b4f30482083141ef7bff3466
Pamphile Roy <roy.pamphile@gmail.com>
2024-05-07 00:05:27 +0200

    Missing main

--- soroban_versioning_pre_push.py
+++ soroban_versioning_pre_push.py
@@ -3,31 +3,37 @@ import subprocess
 
 import soroban
 
-project_key = os.getenv("SVN_PROJECT_KEY")
-project_key = bytes.fromhex(project_key)
 
-commit_hash = subprocess.run(
-    ["git", "rev-parse", "HEAD"],
-    capture_output=True
-).stdout.decode().split('\n')[0]
-commit_hash = bytes.fromhex(commit_hash)
+def main():
+    project_key = os.getenv("SVN_PROJECT_KEY")
+    project_key = bytes.fromhex(project_key)
 
-contract_id = os.getenv(
-    "SVN_CONTRACT_ID",
-    "CAHCQFBMZIY6Y6QPHPN2N64QVKIA6CTGTWBS3SNNIRCATRBANAV3NHWK"
-)
+    commit_hash = subprocess.run(
+        ["git", "rev-parse", "HEAD"],
+        capture_output=True
+    ).stdout.decode().split('\n')[0]
+    commit_hash = bytes.fromhex(commit_hash)
 
-source_account = soroban.Identity()
-args = [
-    {"name": "maintainer", "type": "address", "value": source_account.public_key},
-    {"name": "project_key", "type": "bytes", "value": project_key},
-    {"name": "hash", "type": "bytes", "value": commit_hash},
-]
-args = soroban.Parameters(args=args).model_dump()
+    contract_id = os.getenv(
+        "SVN_CONTRACT_ID",
+        "CAHCQFBMZIY6Y6QPHPN2N64QVKIA6CTGTWBS3SNNIRCATRBANAV3NHWK"
+    )
 
-soroban.invoke(
-    contract_id=contract_id,
-    function_name="commit",
-    args=args,
-    source_account=source_account
-)
+    source_account = soroban.Identity()
+    args = [
+        {"name": "maintainer", "type": "address", "value": source_account.public_key},
+        {"name": "project_key", "type": "bytes", "value": project_key},
+        {"name": "hash", "type": "bytes", "value": commit_hash},
+    ]
+    args = soroban.Parameters(args=args).model_dump()
+
+    soroban.invoke(
+        contract_id=contract_id,
+        function_name="commit",
+        args=args,
+        source_account=source_account
+    )
+
+
+if __name__ == "__main__":
+    main()
