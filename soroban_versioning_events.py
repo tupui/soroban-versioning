32f9a2385c2464997086c89cb7cbc8eb4da3e029
Pamphile Roy <roy.pamphile@gmail.com>
2024-05-10 18:58:23 +0200

    Cleanup events script

--- soroban_versioning_events.py
+++ soroban_versioning_events.py
@@ -1,76 +1,53 @@
-import json
-import uuid
+import binascii
+from collections import namedtuple
 
-import httpx
+import stellar_sdk
+import stellar_sdk.xdr
+from stellar_sdk.soroban_rpc import EventFilter, EventFilterType
+
+Event = namedtuple("Event", ["topics", "value"])
 
 
-def events(contract_id: str, start_ledger: int | None = None):
-    with httpx.Client() as session:
-        if start_ledger is None:
-            res = httpx.post(
-                "https://soroban-testnet.stellar.org",
-                json={"jsonrpc": "2.0", "id": uuid.uuid4().hex,
-                      "method": "getLatestLedger"},
+def parse_xdr(xdr):
+    scval = stellar_sdk.xdr.SCVal.from_xdr(xdr)
+    type_scval = scval.type.name.split("_")[1].lower()
+    parsed_scval = getattr(stellar_sdk.scval, f"from_{type_scval}")(scval)
+
+    if isinstance(parsed_scval, bytes):
+        parsed_scval = binascii.b2a_hex(parsed_scval)
+    return parsed_scval
+
+
+def fetch_events(contract_id: str, start_ledger: int | None = None):
+    soroban_server = stellar_sdk.SorobanServer()
+    if start_ledger is None:
+        last_ledger = soroban_server.get_latest_ledger().sequence
+        start_ledger = int(last_ledger - (3600 / 6 * 20))  # around 20h back
+
+    res = soroban_server.get_events(
+        start_ledger,
+        filters=[
+            EventFilter(
+                event_type=EventFilterType.CONTRACT,
+                contract_ids=[contract_id],
+                # topics=[["*", "*"], ],
             )
-            res.raise_for_status()
-            last_ledger = res.json()["result"]["sequence"]
-            start_ledger = int(last_ledger-(3600 / 6 * 20))  # around 20h back
+        ],
+    )
 
-        res = session.post(
-            "https://soroban-testnet.stellar.org",
-            json={
-                "jsonrpc": "2.0",
-                "id": uuid.uuid4().hex,
-                "method": "getEvents",
-                "params": {
-                    "startLedger": start_ledger,
-                    "filters": [
-                        {
-                            "type": "contract",
-                            "contractIds": [contract_id],
-                            "topics": [["*", "*"]],
-                        }
-                    ],
-                },
-            },
-        )
-        res.raise_for_status()
-        res = res.json()  # ["result"]
-        print(json.dumps(res, indent=4))
+    latest_ledger = res.latest_ledger
+    events_ = []
+    for event in res.events:
+        topics = [parse_xdr(topic) for topic in event.topic]
+        value = parse_xdr(event.value)
+        events_.append(Event(topics=topics, value=value))
+
+    return events_, latest_ledger
 
 
 contract_id = "CAHCQFBMZIY6Y6QPHPN2N64QVKIA6CTGTWBS3SNNIRCATRBANAV3NHWK"
-start_ledger = None  # 1499300
+start_ledger = None
 
-events(contract_id=contract_id, start_ledger=start_ledger)
+events = fetch_events(contract_id=contract_id, start_ledger=start_ledger)
 
-# import stellar_sdk
-# from stellar_sdk.soroban_rpc import EventFilter, EventFilterType
-#
-# soroban_server = stellar_sdk.SorobanServer()
-# soroban_server.get_events(
-#     1499300, filters=[
-#         EventFilter(
-#             event_type=EventFilterType.CONTRACT,
-#             contract_ids=[
-#                 "CAHCQFBMZIY6Y6QPHPN2N64QVKIA6CTGTWBS3SNNIRCATRBANAV3NHWK"
-#             ],
-#             topics=[
-#                 ["*", "*"],
-#             ],
-#         )
-#     ]
-# )
-
-
-# import binascii
-
-# 'AAAADwAAAAZjb21taXQAAA=='
-# 'AAAADQAAACCa/N5K2SsdROdFe/OAy7D47x6z81F+57cvQ763w7wCrA=='
-# 'AAAADQAAABTF7kUFjfiSWLEGkJ0tlC0w0SMASA=='
-
-# stellar_sdk.xdr.sc_val.SCVal.from_xdr().bytes.sc_bytes
-# binascii.b2a_hex()
-
-# stellar_sdk.xdr.sc_val.SCVal.from_xdr().sym.sc_symbol
-# decode
+print(events)
