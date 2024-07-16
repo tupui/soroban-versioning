6663520bd9e6ede248fef8157b2af0b6b6b41046
Tupui <23188539+tupui@users.noreply.github.com>
2024-07-17 00:06:51 +0200

    Use model in listener

--- tansu/src/tansu/events/consume.py
+++ tansu/src/tansu/events/consume.py
@@ -1,14 +1,16 @@
 import sqlalchemy
-import stellar_sdk
 
+from tansu.events import models
 from tansu.events.database import db_models
 from tansu.events.log import logger
 
 
 @sqlalchemy.event.listens_for(db_models.Event, "after_insert")
 def event_handler(mapper, connection, target: db_models.Event):
-    project_key = stellar_sdk.scval.to_native(target.project_key)
-    action = stellar_sdk.scval.to_native(target.action)
-    value = stellar_sdk.scval.to_native(target.value)
+    event = models.Event(
+        project_key=target.project_key, action=target.action, value=target.value
+    )
 
-    logger.info(f"Event listener: {project_key} :: {action} :: {value}")
+    logger.info(
+        f"Event listener: {event.project_key} :: {event.action} :: {event.value}"
+    )
