bb14861230cdbe8b3a45c399985940ec13ec0a38
Tupui <23188539+tupui@users.noreply.github.com>
2024-08-19 19:30:59 +0200

    Fix default env vars

--- dapp/src/contracts/util.ts
+++ dapp/src/contracts/util.ts
@@ -1,2 +1,2 @@
-export const rpcUrl = import.meta.env.PUBLIC_SOROBAN_RPC_URL ?? "http://localhost:8000/rpc"
-export const networkPassphrase = import.meta.env.PUBLIC_SOROBAN_NETWORK_PASSPHRASE ?? "Standalone Network ; February 2017"
+export const rpcUrl = import.meta.env.PUBLIC_SOROBAN_RPC_URL ?? "https://soroban-testnet.stellar.org:443"
+export const networkPassphrase = import.meta.env.PUBLIC_SOROBAN_NETWORK_PASSPHRASE ?? "Test SDF Network ; September 2015"
