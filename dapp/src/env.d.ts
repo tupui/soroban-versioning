f978387328bd8d538eb75c3714cd0136e6aef313
Tupui <23188539+tupui@users.noreply.github.com>
2024-08-22 20:21:54 +0200

    Move card to project

--- dapp/src/env.d.ts
+++ dapp/src/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
+  readonly TANSU_CONTRACT_ID: string
   readonly SOROBAN_NETWORK: string
   readonly SOROBAN_NETWORK_PASSPHRASE: string;
   readonly SOROBAN_RPC_URL: string;
