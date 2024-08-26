df4e0fb7cef1eabb12ef7cc3ee96e5758b30509a
Tupui <23188539+tupui@users.noreply.github.com>
2024-08-26 20:36:04 +0200

    Add register

--- dapp/src/components/project.ts
+++ dapp/src/components/project.ts
@@ -70,4 +70,43 @@ async function commitHash(commit_hash: string): Promise<boolean> {
   }
 }
 
-export { commitHash, getProjectHash, loadedProjectId, setProjectId };
+async function registerProject(maintainers: string, config_url: string, config_hash: string, domain_contract_id: string): Promise<boolean> {
+  if (!projectState.project_id) {
+    alert("No project defined");
+    return false;
+  }
+  const publicKey = loadedPublicKey();
+
+  if (!publicKey) {
+    alert("Please connect your wallet first");
+    return false;
+  } else {
+    Versioning.options.publicKey = publicKey;
+  }
+
+  const maintainers_ = maintainers.split(",");
+
+  const tx = await Versioning.register({
+    // @ts-ignore
+    name: projectState.project_name,
+    maintainer: publicKey,
+    maintainers: maintainers_,
+    url: config_url,
+    hash: config_hash,
+    domain_contract_id: domain_contract_id,
+  });
+  try {
+    await tx.signAndSend({
+      signTransaction: async (xdr) => {
+        const { signedTxXdr } = await kit.signTransaction(xdr);
+        return signedTxXdr;
+      },
+    });
+    return true;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+}
+
+export { commitHash, getProjectHash, loadedProjectId, registerProject, setProjectId };
--- dapp/src/env.d.ts
+++ dapp/src/env.d.ts
@@ -1,9 +1,14 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
+  readonly SOROBAN_DOMAIN_CONTRACT_ID: string;
   readonly TANSU_CONTRACT_ID: string;
   readonly SOROBAN_NETWORK: string;
   readonly SOROBAN_NETWORK_PASSPHRASE: string;
   readonly SOROBAN_RPC_URL: string;
   readonly SOROBAN_SOURCE_ACCOUNT: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
