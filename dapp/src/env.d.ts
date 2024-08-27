e5ab3ec0227cfecd2597eb935b38b44aa358fc3c
Tupui <23188539+tupui@users.noreply.github.com>
2024-08-27 17:40:12 +0200

    Fix env variable handling with public

--- dapp/src/env.d.ts
+++ dapp/src/env.d.ts
@@ -1,12 +1,12 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly SOROBAN_DOMAIN_CONTRACT_ID: string;
-  readonly TANSU_CONTRACT_ID: string;
-  readonly SOROBAN_NETWORK: string;
-  readonly SOROBAN_NETWORK_PASSPHRASE: string;
-  readonly SOROBAN_RPC_URL: string;
+  readonly PUBLIC_SOROBAN_DOMAIN_CONTRACT_ID: string;
+  readonly PUBLIC_TANSU_CONTRACT_ID: string;
+  readonly PUBLIC_SOROBAN_NETWORK_PASSPHRASE: string;
+  readonly PUBLIC_SOROBAN_RPC_URL: string;
   readonly SOROBAN_SOURCE_ACCOUNT: string;
+  readonly SOROBAN_NETWORK: string;
 }
 
 interface ImportMeta {
