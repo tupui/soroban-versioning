723f81d57ee5b5e681d42cf0a5deb476d62116e0
Tupui <23188539+tupui@users.noreply.github.com>
2024-08-28 15:11:28 +0200

    Add some backend tests

--- tansu/tests/conftest.py
+++ tansu/tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
+from httpx import AsyncClient, ASGITransport
 from sqlalchemy import create_engine
 
+from tansu.events.app import create_app
 from tansu.events.database import db_models
 from tansu.events.database.session_factory import (
     conn_str,
@@ -8,6 +10,27 @@ from tansu.events.database.session_factory import (
     SessionFactory,
 )
 from tansu.events.ingest import events_to_db
+
+
+TANSU_PROJECT_KEY = "37ae83c06fde1043724743335ac2f3919307892ee6307cce8c0c63eaa549e156"
+
+
+# ----- API -----
+
+
+@pytest.fixture(scope="session", autouse=True)
+def app():
+    app = create_app()
+    yield app
+
+
+@pytest.fixture(scope="session")
+async def a_client(app):
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as c:
+        yield c
+
 
 # ----- DATABASE -----
 
@@ -39,13 +62,13 @@ async def fake_events(tables):
     event_1 = db_models.Event(
         ledger=4,
         action="commit",
-        project_key="37ae83c06fde1043724743335ac2f3919307892ee6307cce8c0c63eaa549e156",
+        project_key=TANSU_PROJECT_KEY,
         value="04cea5e23c7c50ae3dc304218314f21e7164c9d2",
     )
     event_2 = db_models.Event(
         ledger=1,
         action="register",
-        project_key="37ae83c06fde1043724743335ac2f3919307892ee6307cce8c0c63eaa549e156",
+        project_key=TANSU_PROJECT_KEY,
         value="bed45eef20315c731cc5912c7e2aa626b7cf2a45",
     )
 
