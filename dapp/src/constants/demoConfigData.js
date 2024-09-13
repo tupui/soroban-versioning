3d2c91a16f9eaa71bf2a7c048dacb33600d0c76f
0xExp-po <xtinc.exp@gmail.com>
2024-09-13 09:40:47 +0900

    chore: update demo config data

--- dapp/src/constants/demoConfigData.js
+++ dapp/src/constants/demoConfigData.js
@@ -15,80 +15,42 @@ export const demoConfigData = [
     ],
   },
   {
-    projectName: "NFT Marketplace",
-    description: "Buy, sell, and trade unique digital assets on our decentralized platform. Discover rare collectibles, showcase your creations, and join a thriving community of artists and collectors.",
-    companyName: "NFT World",
+    projectName: "scipy",
+    description: "SciPy library main repository",
+    companyName: "SciPy",
     officials: {
-      websiteLink: "https://nftmarketplace.example.com",
-      githubLink: "https://github.com/nftmarketplace",
+      websiteLink: "https://scipy.org/",
+      githubLink: "https://github.com/scipy/scipy",
     },
     socialLinks: {
-      twitter: "https://twitter.com/nftmarketplace",
-      telegram: "https://t.me/nftmarketplace",
-      discord: "https://discord.gg/nftmarketplace",
-      instagram: "https://instagram.com/nftmarketplace",
+      twitter: "https://twitter.com/",
+      telegram: "https://t.me/",
+      discord: "https://discord.gg/",
+      instagram: "https://instagram.com/",
     },
-    authorGithubNames: ["charlie", "david"],
+    authorGithubNames: ["rgommers", "pv"],
     maintainersAddresses: [
       "GAJCCCRIRR7OQBEMWRYSUKNXQOHQXAMCPBXJYZD3kBLy6KPWxYv3W2C",
       "GDNSSYSCSSJ76FER5WEEXME5G4MTCUBKDRQSKOYP36KUKVDB2VCMERS6"
     ],
   },
   {
-    projectName: "Yield Farming",
-    logoImageLink: "https://example.com/yield-farming-logo.png",
-    description: "Earn rewards by staking your crypto assets",
-    companyName: "Yield Boost",
+    projectName: "salib",
+    logoImageLink: "https://raw.githubusercontent.com/SALib/SALib/main/docs/assets/logo.png",
+    description: "Python implementations of commonly used sensitivity analysis methods. Useful in systems modeling to calculate the effects of model inputs or exogenous factors on outputs of interest.",
+    companyName: " SALib",
     officials: {
-      githubLink: "https://github.com/yieldfarming",
+      websiteLink: "http://salib.github.io/SALib/",
+      githubLink: "https://github.com/SALib/SALib",
     },
     socialLinks: {
-      twitter: "https://twitter.com/yieldfarming",
-      instagram: "https://instagram.com/yieldfarming",
+      twitter: "https://twitter.com/",
+      instagram: "https://instagram.com/",
     },
-    authorGithubNames: ["eve", "frank"],
+    authorGithubNames: ["ConnectedSystems","tupui"],
     maintainersAddresses: [
       "GCUDW6ZF5SCGCMS3QUTELZ6LSAH6IVVXNRPRLAUNJ2XYLCA7KH7ZCVQS",
       "GDQP2KPQGKIHYJGXNUIYOMHARUARCA7DJT5FO2FFOOKY3B2WSQHG4W37"
-    ],
-  },
-  {
-    projectName: "Decentralized Exchange",
-    logoImageLink: "https://example.com/dex-logo.png",
-    description: "Trade cryptocurrencies without intermediaries",
-    companyName: "DEX Solutions",
-    officials: {
-      websiteLink: "https://dex.example.com",
-      githubLink: "https://github.com/dexsolutions",
-    },
-    socialLinks: {
-      twitter: "https://twitter.com/dexsolutions",
-      telegram: "https://t.me/dexsolutions",
-      discord: "https://discord.gg/dexsolutions",
-    },
-    authorGithubNames: ["grace", "henry"],
-    maintainersAddresses: [
-      "GBQ6RTBMO5SKENOKU5FZIVIQQ7IQHPNBZXL3PPRFKBA2XDAQM4UKXPVN",
-      "GAAZI4TCR3TY5OJHCTJC2A4QSY6CJWJH5IAJTGKIN2ER7LBNVKOCCWN7"
-    ],
-  },
-  {
-    projectName: "Crypto Wallet",
-    logoImageLink: "https://example.com/crypto-wallet-logo.png",
-    description: "Secure storage and management of cryptocurrencies",
-    companyName: "Wallet Secure",
-    officials: {
-      websiteLink: "https://cryptowallet.example.com",
-      githubLink: "https://github.com/cryptowallet",
-    },
-    socialLinks: {
-      twitter: "https://twitter.com/cryptowallet",
-      telegram: "https://t.me/cryptowallet",
-    },
-    authorGithubNames: ["isabel", "jack"],
-    maintainersAddresses: [
-      "GDXFAGJCSCI4CK2ENOAA4CHJZJNXF4YQRRKJKE4JRIHFTIBKFVJRPXIC",
-      "GA6RUD4DZ2NEMAQY4VZJ4C6K6VSEYEJITNSLUQKLCFHJ2JOGC5UCGCFQ"
     ],
   },
 ];
