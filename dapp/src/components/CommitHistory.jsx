45715e9d105b37062be24b7826c80654de0bd61f
0xExp-po <lucaschang1127@gmail.com>
2024-09-15 22:53:13 +0900

    chore: put pagination out from scroll area

--- dapp/src/components/CommitHistory.jsx
+++ dapp/src/components/CommitHistory.jsx
@@ -49,30 +49,32 @@ const CommitHistory = () => {
   }, [isProjectInfoLoaded]);
 
   return (
-    <div className="commit-history-container max-h-[560px] overflow-auto relative pl-8">
-      {commitHistory.map((day) => (
-        <div key={day.date} className="day-group relative">
-          <h3 className="text-lg font-semibold pb-4 relative before:content-[''] before:absolute before:-left-[1.5rem] before:top-0 before:bottom-0 before:w-0.5 before:bg-zinc-700">
-            <div className="commit-dot absolute left-[-2.2rem] top-0 w-[25px] h-[25px] bg-[#f3f3f3] rounded-full before:content-[''] before:absolute before:left-[50%] before:top-[50%] before:transform before:-translate-x-1/2 before:-translate-y-1/2 before:w-[15px] before:h-[15px] before:border-[2px] before:rounded-full"></div>
-            <span className="pr-2 relative z-10">{formatDate(day.date)}</span>
-          </h3>
-          <div className="space-y-4 pb-4 relative before:content-[''] before:absolute before:-left-[1.5rem] before:top-0 before:bottom-0 before:w-0.5 before:bg-zinc-700">
-            {day.commits.map((commit) => (
-              <div key={commit.sha} className="relative">
-                <CommitRecord
-                  message={commit.message}
-                  date={commit.commit_date}
-                  authorName={commit.author.name}
-                  authorGithubLink={commit.author.html_url}
-                  sha={commit.sha}
-                  commitLink={commit.html_url}
-                  isMaintainer={authors ? authors.includes(commit.author.name.toLowerCase()) : false}
-                />
-              </div>
-            ))}
+    <>
+      <div className="commit-history-container max-h-[560px] overflow-auto relative pl-8">
+        {commitHistory.map((day) => (
+          <div key={day.date} className="day-group relative">
+            <h3 className="text-lg font-semibold pb-4 relative before:content-[''] before:absolute before:-left-[1.5rem] before:top-0 before:bottom-0 before:w-0.5 before:bg-zinc-700">
+              <div className="commit-dot absolute left-[-2.2rem] top-0 w-[25px] h-[25px] bg-[#f3f3f3] rounded-full before:content-[''] before:absolute before:left-[50%] before:top-[50%] before:transform before:-translate-x-1/2 before:-translate-y-1/2 before:w-[15px] before:h-[15px] before:border-[2px] before:rounded-full"></div>
+              <span className="pr-2 relative z-10">{formatDate(day.date)}</span>
+            </h3>
+            <div className="space-y-4 pb-4 relative before:content-[''] before:absolute before:-left-[1.5rem] before:top-0 before:bottom-0 before:w-0.5 before:bg-zinc-700">
+              {day.commits.map((commit) => (
+                <div key={commit.sha} className="relative">
+                  <CommitRecord
+                    message={commit.message}
+                    date={commit.commit_date}
+                    authorName={commit.author.name}
+                    authorGithubLink={commit.author.html_url}
+                    sha={commit.sha}
+                    commitLink={commit.html_url}
+                    isMaintainer={authors ? authors.includes(commit.author.name.toLowerCase()) : false}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
       
       {/* Pagination  */}
       <div className="flex justify-center items-center mt-4 space-x-2">
@@ -111,7 +113,7 @@ const CommitHistory = () => {
           </svg>
         </button>
       </div>
-    </div>
+    </>
   );
 };
 
