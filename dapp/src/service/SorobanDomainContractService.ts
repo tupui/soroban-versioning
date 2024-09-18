c15c2c46988feaf3bdfe11576d6e37317c0e3a82
0xExp-po <lucaschang1127@gmail.com>
2024-09-18 12:15:15 +0900

    feat: implement donate function

---
+++ dapp/src/service/SorobanDomainContractService.ts
@@ -0,0 +1,28 @@
+import type { Record } from '@creit.tech/sorobandomains-sdk';
+import { Domain404Error } from '@creit.tech/sorobandomains-sdk';
+import sdk from '../contracts/soroban_domain_sdk';
+
+async function getAddressFromDomain (
+  domainName: string,
+) {
+  try {
+    const domainRecord: Record = await sdk.searchDomain({ domain: domainName });
+    return domainRecord;
+  } catch (e) {
+    if (e instanceof Error && e.name === Domain404Error.name) {
+      return {
+        "error": true,
+        "message": "SDK error",
+      }
+    } else {
+      return {
+        "error": true,
+        "message": "Simulation error",
+      }
+    }
+  }
+}
+
+export {
+  getAddressFromDomain,
+}
