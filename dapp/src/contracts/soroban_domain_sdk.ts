360bce23dc2fd01e61ba05f536b4a83cbb8687e7
0xExp-po <153439271+0xExp-po@users.noreply.github.com>
2024-09-21 07:36:24 +0900

    Dapp add devote (#56)

---
+++ dapp/src/contracts/soroban_domain_sdk.ts
@@ -0,0 +1,21 @@
+import * as SDK from '@stellar/stellar-sdk';
+import { SorobanDomainsSDK } from '@creit.tech/sorobandomains-sdk';
+
+const rpcUrl = import.meta.env.PUBLIC_SOROBAN_RPC_URL;
+const networkPassphrase = import.meta.env.PUBLIC_SOROBAN_NETWORK_PASSPHRASE;
+const contractId = import.meta.env.PUBLIC_SOROBAN_DOMAIN_CONTRACT_ID;
+const defaultFee = import.meta.env.PUBLIC_DEFAULT_FEE;
+const defaultTimeout = import.meta.env.PUBLIC_DEFAULT_TIMEOUT;
+const simulationAccount = import.meta.env.PUBLIC_TANSU_OWNER_ID;
+
+const sdk: SorobanDomainsSDK = new SorobanDomainsSDK({
+  stellarSDK: SDK,
+  rpc: new SDK.SorobanRpc.Server(rpcUrl || 'https://soroban-testnet.stellar.org:443'),
+  network: (networkPassphrase as SDK.Networks) || SDK.Networks.TESTNET,
+  contractId: contractId || '',
+  defaultFee: defaultFee || '100',
+  defaultTimeout: parseInt(defaultTimeout || '30'),
+  simulationAccount: simulationAccount || '',
+});
+
+export default sdk;
---
+++ dapp/src/service/PaymentService.ts
@@ -0,0 +1,66 @@
+import { kit, loadedPublicKey } from "../components/stellar-wallets-kit";
+import * as StellarSdk from "@stellar/stellar-sdk";
+
+async function sendXLM(donateAmount: string, projectAddress: string, tipAmount: string, donateMessage: string): Promise<boolean> {
+  const senderPublicKey = loadedPublicKey();
+
+  const tansuAddress = import.meta.env.PUBLIC_TANSU_OWNER_ID;
+
+  if (!senderPublicKey) {
+    alert("Please connect your wallet first");
+    return false;
+  }
+
+  try {
+    // Fetch the sender's account details from the Stellar network
+    const server = new StellarSdk.Horizon.Server("https://horizon-testnet.stellar.org");
+    const account = await server.loadAccount(senderPublicKey);
+    
+    // Create the transaction
+    const transactionBuilder = new StellarSdk.TransactionBuilder(account, {
+      fee: StellarSdk.BASE_FEE,
+      networkPassphrase: StellarSdk.Networks.TESTNET,
+    })
+    .addOperation(StellarSdk.Operation.payment({
+      destination: projectAddress,
+      asset: StellarSdk.Asset.native(),
+      amount: donateAmount,
+    }))
+    .addMemo(StellarSdk.Memo.text(donateMessage));
+    
+    // Add tip operation only if tipAmount is not "0"
+    if (tipAmount !== "0") {
+      transactionBuilder.addOperation(StellarSdk.Operation.payment({
+        destination: tansuAddress,
+        asset: StellarSdk.Asset.native(),
+        amount: tipAmount,
+      }));
+    }
+
+    const transaction = transactionBuilder
+      .setTimeout(StellarSdk.TimeoutInfinite)
+      .build();
+
+      // Sign and send the transaction
+      const { signedTxXdr } = await kit.signTransaction(transaction.toXDR());
+      
+    try {
+      const result = await server.submitTransaction(
+        StellarSdk.TransactionBuilder.fromXDR(signedTxXdr, StellarSdk.Networks.TESTNET)
+      );
+      console.log("Transaction successful:", JSON.stringify(result, null, 2));
+      return true;
+    } catch (error) {
+      console.error("Error response from Stellar:", JSON.stringify(error, null, 2));
+      return false;
+    }
+    
+  } catch (error) {
+    console.error("Error sending XLM:", JSON.stringify(error, null, 2));
+    return false;
+  }
+}
+
+export {
+  sendXLM,
+};
