7daa479cb1e67f26b5dee0e240d5aedc86c7b097
Tupui <23188539+tupui@users.noreply.github.com>
2024-09-22 18:02:49 +0200

    Clean domain SDK init

--- dapp/src/contracts/soroban_domain_sdk.ts
+++ dapp/src/contracts/soroban_domain_sdk.ts
@@ -1,21 +1,20 @@
 import * as SDK from '@stellar/stellar-sdk';
 import { SorobanDomainsSDK } from '@creit.tech/sorobandomains-sdk';
+import { rpcUrl, networkPassphrase } from "./util";
 
-const rpcUrl = import.meta.env.PUBLIC_SOROBAN_RPC_URL;
-const networkPassphrase = import.meta.env.PUBLIC_SOROBAN_NETWORK_PASSPHRASE;
 const contractId = import.meta.env.PUBLIC_SOROBAN_DOMAIN_CONTRACT_ID;
-const defaultFee = import.meta.env.PUBLIC_DEFAULT_FEE;
-const defaultTimeout = import.meta.env.PUBLIC_DEFAULT_TIMEOUT;
+const defaultFee = import.meta.env.PUBLIC_DEFAULT_FEEL ?? "100";
+const defaultTimeout = import.meta.env.PUBLIC_DEFAULT_TIMEOUT ?? "30";
 const simulationAccount = import.meta.env.PUBLIC_TANSU_OWNER_ID;
 
 const sdk: SorobanDomainsSDK = new SorobanDomainsSDK({
   stellarSDK: SDK,
-  rpc: new SDK.SorobanRpc.Server(rpcUrl || 'https://soroban-testnet.stellar.org:443'),
-  network: (networkPassphrase as SDK.Networks) || SDK.Networks.TESTNET,
-  contractId: contractId || '',
-  defaultFee: defaultFee || '100',
-  defaultTimeout: parseInt(defaultTimeout || '30'),
-  simulationAccount: simulationAccount || '',
+  rpc: new SDK.SorobanRpc.Server(rpcUrl),
+  network: networkPassphrase,
+  contractId: contractId,
+  defaultFee: defaultFee,
+  defaultTimeout: parseInt(defaultTimeout),
+  simulationAccount: simulationAccount,
 });
 
 export default sdk;
