e8db278db7b2b8cdf486d3a505c791c592f39b55
Tupui <23188539+tupui@users.noreply.github.com>
2024-09-26 23:45:03 +0200

    Try fix for main site social card fix

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -56,7 +56,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/tansu-social-card.png',
+    image: 'https://tansu.dev/img/tansu-social-card.png',
     navbar: {
       title: 'Tansu',
       logo: {
