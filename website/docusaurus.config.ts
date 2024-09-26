3c747e0fc4584559abbb7e77495fcc392ca0c16f
Tupui <23188539+tupui@users.noreply.github.com>
2024-09-26 22:27:54 +0200

    Fix social card

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -56,7 +56,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/tansu-social-card.jpg',
+    image: 'img/tansu-social-card.png',
     navbar: {
       title: 'Tansu',
       logo: {
