d6f4a59455d91e5bf3ddd5ed798fc0eee050e83c
0xExp-po <lucaschang1127@gmail.com>
2024-10-07 18:25:36 +0900

    chore: change SSR to CSR

--- dapp/astro.config.mjs
+++ dapp/astro.config.mjs
@@ -2,11 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'server',
-  adapter: netlify(),
 });
