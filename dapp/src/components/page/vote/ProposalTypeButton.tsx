4d0b0abe798189ec68bd8c041bcff147d35d94df
0xExp-po <xtinc.exp@gmail.com>
2024-11-13 18:28:12 +0900

    chore: update some topic style

--- dapp/src/components/page/vote/ProposalTypeButton.tsx
+++ dapp/src/components/page/vote/ProposalTypeButton.tsx
@@ -26,8 +26,8 @@ const ProposalTypeButton: React.FC<ProposalTypeButtonProps> = ({
     }
   }, [proposalTypeStatus]);
 
-  const buttonClass = `px-2.5 sm:px-3 py-1.5 sm:py-2 text-md sm:text-xl font-medium border border-zinc-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white ${
-    isSelected ? "bg-zinc-500" : "bg-white"
+  const buttonClass = `px-2.5 sm:px-3 py-1.5 sm:py-2 text-md sm:text-xl font-medium border border-zinc-200 focus:outline-none ${
+    isSelected ? "bg-zinc-200" : "bg-white"
   } ${position === "start" ? "rounded-tl-lg" : position === "end" ? "rounded-tr-lg" : ""}`;
 
   return (
