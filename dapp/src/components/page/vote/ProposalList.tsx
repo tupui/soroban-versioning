ee0a170a36a251ab72b10d25b602997eec6cfa6d
0xExp-po <xtinc.exp@gmail.com>
2024-11-13 11:19:59 +0900

    chore: create & add pagination component to proposal section

---
+++ dapp/src/components/page/vote/ProposalList.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useState } from "react";
+import Pagination from "../../utils/Pagination";
+
+const ProposalList: React.FC = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  return (
+    <div className="w-full py-6 sm:py-8 md:py-12 px-7 sm:px-9 md:px-13">
+      <div className="flex flex-col gap-2 sm:gap-3 md:gap-5"></div>
+      <div>
+        <Pagination
+          currentPage={1}
+          onPageChange={(page: number) => setCurrentPage(page)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProposalList;
