fe4ea85b137588d0e75012fbb3d66d7054e47662
0xExp-po <xtinc.exp@gmail.com>
2024-11-14 10:06:27 +0900

    chore: add project name as query param in governance page

--- dapp/src/utils/store.js
+++ dapp/src/utils/store.js
@@ -3,3 +3,4 @@ import { atom } from "nanostores";
 export const projectInfoLoaded = atom(false);
 export const latestCommit = atom("");
 export const projectCardModalOpen = atom(false);
+export const projectNameForGovernance = atom("");
