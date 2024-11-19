6bbf409ca92a73b03657d6ac3a31d33f801cb045
0xExp-po <xtinc.exp@gmail.com>
2024-11-19 13:35:47 +0900

    chore: hover effect to xlm logo

--- dapp/src/components/page/proposal/ProposalDetail.tsx
+++ dapp/src/components/page/proposal/ProposalDetail.tsx
@@ -101,13 +101,25 @@ export const OutcomeDetail: React.FC<{
               href={`${stellarLabViewXdrLink}${modifySlashInXdr(detail.xdr)};;`}
               target="_blank"
               rel="noreferrer"
-              className="flex gap-0.5 sm:gap-1 text-black hover:text-blue"
+              className="flex items-center gap-0.5 sm:gap-1 text-black hover:text-blue  group"
             >
-              <img
-                src="/icons/logos/stellar-xlm.svg"
-                alt="Stellar"
-                className="w-4 sm:w-[20px] h-4 sm:h-[20px]"
-              />
+              <svg
+                role="img"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                id="Stellar--Streamline-Simple-Icons.svg"
+                height="24"
+                width="24"
+                fill="#000000"
+                className="group-hover:fill-blue w-3 h-3 sm:w-4 sm:h-4 md:w-5 md:h-5"
+              >
+                <desc>Stellar Streamline Icon: https://streamlinehq.com</desc>
+                <title>Stellar</title>
+                <path
+                  d="M12.283 1.851A10.154 10.154 0 0 0 1.846 12.002c0 0.259 0.01 0.516 0.03 0.773A1.847 1.847 0 0 1 0.872 14.56L0 15.005v2.074l2.568 -1.309 0.832 -0.424 0.82 -0.417 14.71 -7.496 1.653 -0.842L24 4.85V2.776l-3.387 1.728 -2.89 1.473 -13.955 7.108a8.376 8.376 0 0 1 -0.07 -1.086 8.313 8.313 0 0 1 12.366 -7.247l1.654 -0.843 0.247 -0.126a10.154 10.154 0 0 0 -5.682 -1.932zM24 6.925 5.055 16.571l-1.653 0.844L0 19.15v2.072L3.378 19.5l2.89 -1.473 13.97 -7.117a8.474 8.474 0 0 1 0.07 1.092A8.313 8.313 0 0 1 7.93 19.248l-0.101 0.054 -1.793 0.914a10.154 10.154 0 0 0 16.119 -8.214c0 -0.26 -0.01 -0.522 -0.03 -0.78a1.848 1.848 0 0 1 1.003 -1.785L24 8.992Z"
+                  stroke-width="1"
+                ></path>
+              </svg>
               <span className="text-xs sm:text-sm md:text-base">
                 View in Stellar Lab
               </span>
