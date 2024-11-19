8267df2213533bb0201a091b05fe1e98c09b8dcf
0xExp-po <xtinc.exp@gmail.com>
2024-11-19 13:36:17 +0900

    linter: svg formatter

--- dapp/.prettierrc.mjs
+++ dapp/.prettierrc.mjs
@@ -3,7 +3,7 @@ export default {
   plugins: ["prettier-plugin-astro"],
   overrides: [
     {
-      files: "*.astro",
+      files: ["*.astro", "*.svg"],
       options: {
         parser: "astro",
       },
