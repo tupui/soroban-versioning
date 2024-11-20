0f379cb9404248cb898836b8d2b388c13befd7f9
0xExp-po <xtinc.exp@gmail.com>
2024-11-20 13:35:25 +0900

    chore: change the description of idea tab

--- dapp/src/components/page/governance/IdeaList.tsx
+++ dapp/src/components/page/governance/IdeaList.tsx
@@ -12,7 +12,7 @@ const IdeaList: React.FC = () => {
     <div className="w-full py-4 sm:py-8 md:py-12 px-3 sm:px-9 md:px-13 bg-zinc-200 rounded-xl sm:rounded-3xl rounded-tl-none sm:rounded-tl-none">
       <div className="flex flex-col gap-2 sm:gap-3 md:gap-5">
         <div className="flex justify-center items-center h-32">
-          <h1 className="text-xl sm:text-2xl md:text-3xl">-- Coming Soon --</h1>
+          <h1 className="text-xl sm:text-2xl md:text-3xl">Stay tuned!</h1>
         </div>
       </div>
       <div>
