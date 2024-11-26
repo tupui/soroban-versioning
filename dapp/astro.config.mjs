6058039d87a96bc2fa58fa0a3fa59f41b8e779cc
0xExp-po <xtinc.exp@gmail.com>
2024-11-26 21:06:12 +0900

    chore: set netlify adapter

--- dapp/astro.config.mjs
+++ dapp/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "hybrid",
+  adapter: netlify(),
 });
