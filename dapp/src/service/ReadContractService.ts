2356f0776a107b513cdabcfcfe8283532c0e859f
0xExp-po <xtinc.exp@gmail.com>
2024-12-04 16:25:37 +0900

    feat: fetch proposals from contract

--- dapp/src/service/ReadContractService.ts
+++ dapp/src/service/ReadContractService.ts
@@ -6,6 +6,11 @@ const { keccak256 } = pkg;
 import { Buffer } from "buffer";
 
 import { loadedProjectId } from "./StateService";
+import type {
+  Proposal as ModifiedProposal,
+  ProposalStatus,
+} from "types/proposal";
+import type { Proposal } from "soroban_versioning";
 
 async function getProjectHash(): Promise<string | void> {
   const projectId = loadedProjectId();
@@ -50,4 +55,74 @@ async function getProjectFromName(
   return res.result;
 }
 
-export { getProject, getProjectHash, getProjectFromName };
+async function getProposals(
+  project_name: string,
+  page: number,
+): Promise<ModifiedProposal[]> {
+  const project_key = Buffer.from(
+    keccak256.create().update(project_name).digest(),
+  );
+  try {
+    const res = await Versioning.get_dao({
+      project_key: project_key,
+      page: page,
+    });
+
+    const proposals: ModifiedProposal[] = res.result.proposals.map(
+      (proposal: Proposal) => {
+        return {
+          id: proposal.id,
+          title: proposal.title,
+          ipfs: proposal.ipfs,
+          nqg: proposal.nqg,
+          status: proposal.status.tag.toLocaleLowerCase() as ProposalStatus,
+          voting_ends_at: Number(proposal.voting_ends_at),
+          voteStatus: {
+            approve: {
+              voteType: "approve",
+              score: proposal.voters_approve.length,
+              voters: proposal.voters_approve.map((voter: string) => {
+                return {
+                  address: voter,
+                  image: null,
+                  name: "",
+                  github: "",
+                };
+              }),
+            },
+            reject: {
+              voteType: "reject",
+              score: proposal.voters_reject.length,
+              voters: proposal.voters_reject.map((voter: string) => {
+                return {
+                  address: voter,
+                  image: null,
+                  name: "",
+                  github: "",
+                };
+              }),
+            },
+            abstain: {
+              voteType: "abstain",
+              score: proposal.voters_abstain.length,
+              voters: proposal.voters_abstain.map((voter: string) => {
+                return {
+                  address: voter,
+                  image: null,
+                  name: "",
+                  github: "",
+                };
+              }),
+            },
+          },
+        };
+      },
+    );
+    return proposals;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+}
+
+export { getProject, getProjectHash, getProjectFromName, getProposals };
