6c8b642b6899bcaf79d4a8a6eb0cf8c71c2602d5
0xExp-po <153439271+0xExp-po@users.noreply.github.com>
2024-12-18 08:46:23 +0900

    Add ledger wallet to kit (#123)

--- dapp/src/components/stellar-wallets-kit.ts
+++ dapp/src/components/stellar-wallets-kit.ts
@@ -3,9 +3,10 @@ import {
   FREIGHTER_ID,
   StellarWalletsKit,
 } from "@creit.tech/stellar-wallets-kit";
+import { LedgerModule } from "@creit.tech/stellar-wallets-kit/modules/ledger.module";
 
 const kit: StellarWalletsKit = new StellarWalletsKit({
-  modules: allowAllModules(),
+  modules: [...allowAllModules(), new LedgerModule()],
   // @ts-ignore
   network: import.meta.env.PUBLIC_SOROBAN_NETWORK_PASSPHRASE,
   selectedWalletId: FREIGHTER_ID,
