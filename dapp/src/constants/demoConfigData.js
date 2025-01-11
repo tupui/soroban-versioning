7685cbe91b0b783fe2b1db9dbd0034779bd7bb8e
0xExp-po <xtinc.exp@gmail.com>
2025-01-11 20:25:58 +0900

    chore: remove-duplicated-projects

--- dapp/src/constants/demoConfigData.js
+++ dapp/src/constants/demoConfigData.js
@@ -31,38 +31,6 @@ export const demoConfigData = [
       "GCMFQP44AR32S7IRIUKNOEJW5PNWOCLRHLQWSHUCSV4QZOMUXZOVA7Q2",
     ],
   },
-  {
-    projectName: "SALib",
-    logoImageLink:
-      "https://raw.githubusercontent.com/SALib/SALib/main/docs/assets/logo.png",
-    description: "Sensitivity Analysis Library in Python",
-    organizationName: "SALib",
-    officials: {
-      websiteLink: "https://salib.readthedocs.io/",
-      githubLink: "https://github.com/SALib/SALib",
-    },
-    socialLinks: {},
-    authorGithubNames: ["tupui"],
-    maintainersAddresses: [
-      "GCMFQP44AR32S7IRIUKNOEJW5PNWOCLRHLQWSHUCSV4QZOMUXZOVA7Q2",
-    ],
-  },
-  {
-    projectName: "tansu",
-    logoImageLink:
-      "https://github.com/tupui/soroban-versioning/blob/main/website/static/img/logo.svg",
-    description: "Bringing Git hashes onto Stellar’s blockchain",
-    organizationName: "Consulting Manao GmbH",
-    officials: {
-      websiteLink: "https://tansu.dev",
-      githubLink: "https://github.com/tupui/soroban-versioning",
-    },
-    socialLinks: {},
-    authorGithubNames: ["tupui"],
-    maintainersAddresses: [
-      "GCMFQP44AR32S7IRIUKNOEJW5PNWOCLRHLQWSHUCSV4QZOMUXZOVA7Q2",
-    ],
-  },
 ];
 
 export function getDemoConfigData() {
