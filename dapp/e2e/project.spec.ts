2f1c5e9436ade2f78eb348540d48175523d32fd3
0xExp-po <xtinc.exp@gmail.com>
2025-01-15 16:53:51 +0900

    feat: add-project-test

---
+++ dapp/e2e/project.spec.ts
@@ -0,0 +1,17 @@
+import { test } from '@playwright/test';
+
+test('go-to-governance', async ({ page }) => {
+    await page.goto('http://localhost:4321/project?name=tansu');
+    await page.getByRole('button', { name: 'Governance ->' }).click();
+});
+
+test('support', async ({ page }) => {
+    await page.goto('http://localhost:4321/project?name=tansu');
+    await page.getByRole('button', { name: '♥ Support' }).click();
+    await page.locator('#donate-modal').click();
+    page.once('dialog', dialog => {
+        console.log(`Dialog message: ${dialog.message()}`);
+        dialog.dismiss().catch(() => { });
+    });
+    await page.getByRole('button', { name: 'Contribute to tansu.xlm' }).click();
+});
