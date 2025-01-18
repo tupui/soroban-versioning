fd009e358c425d15ba9ab6b37c292f2024e3b63e
0xExp-po <xtinc.exp@gmail.com>
2025-01-18 23:44:44 +0900

    chore: config-base-url

--- dapp/playwright.config.ts
+++ dapp/playwright.config.ts
@@ -23,10 +23,12 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
+  /* Timeout in milliseconds */
+  timeout: 240000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: "http://localhost:4321", // You can also fall back to a default value
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
