bc0fcf6848865762d32f71075b7e4495733c0540
0xExp-po <xtinc.exp@gmail.com>
2025-01-18 23:42:45 +0900

    feat: add-constants-for-test

---
+++ dapp/e2e/constants.ts
@@ -0,0 +1,31 @@
+import path from "path";
+
+export const githubRepoURL = "https://github.com/0xExp-po/soroban-versioning";
+export const infoFileHash =
+  "920b7ffed638360e7259c4b6a4691ef947cfb9bc4ab1b3d6b7f0628c71e86b25";
+
+export const walletExtensionPath = path.join(import.meta.dirname, "freighter");
+export const wallet1 = {
+  address: "GAYW7P7VQ6BDSSW4TJOIXX5CITVFG455NQIAMCK22BYUOUHI6CKOAEFV",
+  seeds: [
+    "language",
+    "exit",
+    "health",
+    "note",
+    "dress",
+    "taste",
+    "right",
+    "envelope",
+    "firm",
+    "before",
+    "sick",
+    "adjust",
+  ],
+  password: "abcABC123!@#",
+};
+
+export const wallet2 = {
+  address: "GBBPDTNPIOLTVG6ED5BDSDSODBMSVI5RXFJTSWUVF7XKUVINMCRRJSBA",
+  secretKey: "SCLOQ4C7OACTQWZX3FD3ANX3XM4F4PYGVYZAENS76HBXW3PFMZFXYTSD",
+  password: "abcABC123!@#",
+};
