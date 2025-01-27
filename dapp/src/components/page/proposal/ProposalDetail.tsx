d6a856be8522f0697bcb06d7e3cdedfa032027d8
0xExp-po <xtinc.exp@gmail.com>
2025-01-27 11:59:57 +0900

    feat: update-proposal-page

--- dapp/src/components/page/proposal/ProposalDetail.tsx
+++ dapp/src/components/page/proposal/ProposalDetail.tsx
@@ -1,31 +1,30 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import * as StellarXdr from "utils/stellarXdr";
+import Button from "components/utils/Button";
+import Modal from "components/utils/Modal";
 import Markdown from "markdown-to-jsx";
-import "github-markdown-css";
+import React, { useEffect, useState } from "react";
 import JsonView from "react18-json-view";
-import "react18-json-view/src/style.css";
+import type { ProposalOutcome } from "types/proposal";
+import { parseToLosslessJson } from "utils/passToLosslessJson";
+import * as StellarXdr from "utils/stellarXdr";
 import {
   capitalizeFirstLetter,
-  modifySlashInXdr,
-  getProposalLinkFromIpfs,
   getOutcomeLinkFromIpfs,
+  getProposalLinkFromIpfs,
 } from "utils/utils";
-import { stellarLabViewXdrLink } from "constants/serviceLinks";
-import type { ProposalOutcome, ProposalViewStatus } from "types/proposal";
-import { parseToLosslessJson } from "utils/passToLosslessJson";
+
+import "github-markdown-css";
+import "react18-json-view/src/style.css";
+
 interface ProposalDetailProps {
   ipfsLink: string | null;
   description: string;
   outcome: ProposalOutcome | null;
-  status: ProposalViewStatus | null;
 }
 
 const ProposalDetail: React.FC<ProposalDetailProps> = ({
   ipfsLink,
   description,
   outcome,
-  status,
 }) => {
   const [isReady, setIsReady] = useState(false);
 
@@ -39,74 +38,50 @@ const ProposalDetail: React.FC<ProposalDetailProps> = ({
   }, []);
 
   return (
-    <div className="w-full bg-zinc-100 rounded-xl px-4 sm:px-8 md:px-12 py-4 sm:py-7 md:py-10">
-      <div className="w-full flex flex-col gap-4 sm:gap-6 md:gap-8">
-        <div className="">
-          <div className="flex items-center gap-1 sm:gap-2 md:gap-3">
-            <div className="text-xl sm:text-2xl md:text-3xl font-medium">
+    <div className="flex flex-col gap-12">
+      <div className="flex flex-col gap-6">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-3">
+            <p className="text-2xl font-medium text-primary">
               Proposal Description
-            </div>
+            </p>
             <a
               href={(ipfsLink && getProposalLinkFromIpfs(ipfsLink)) || ""}
               target="_blank"
               rel="noreferrer"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                id="Layer_1"
-                x="0px"
-                y="0px"
-                className="w-4 sm:w-6 h-4 sm:h-6 fill-black hover:fill-blue"
-                viewBox="0 0 512 512"
-                enableBackground="new 0 0 512 512"
-              >
-                <path d="M459.654,233.373l-90.531,90.5c-49.969,50-131.031,50-181,0c-7.875-7.844-14.031-16.688-19.438-25.813  l42.063-42.063c2-2.016,4.469-3.172,6.828-4.531c2.906,9.938,7.984,19.344,15.797,27.156c24.953,24.969,65.563,24.938,90.5,0  l90.5-90.5c24.969-24.969,24.969-65.563,0-90.516c-24.938-24.953-65.531-24.953-90.5,0l-32.188,32.219  c-26.109-10.172-54.25-12.906-81.641-8.891l68.578-68.578c50-49.984,131.031-49.984,181.031,0  C509.623,102.342,509.623,183.389,459.654,233.373z M220.326,382.186l-32.203,32.219c-24.953,24.938-65.563,24.938-90.516,0  c-24.953-24.969-24.953-65.563,0-90.531l90.516-90.5c24.969-24.969,65.547-24.969,90.5,0c7.797,7.797,12.875,17.203,15.813,27.125  c2.375-1.375,4.813-2.5,6.813-4.5l42.063-42.047c-5.375-9.156-11.563-17.969-19.438-25.828c-49.969-49.984-131.031-49.984-181.016,0  l-90.5,90.5c-49.984,50-49.984,131.031,0,181.031c49.984,49.969,131.031,49.969,181.016,0l68.594-68.594  C274.561,395.092,246.42,392.342,220.326,382.186z" />
-              </svg>
+              <img src="/icons/link.svg" />
             </a>
           </div>
-          <div className="w-full mt-4 sm:mt-5 md:mt-7 min-h-24 sm:min-h-32">
-            <div className="markdown-body w-full px-4 sm:px-6 md:px-8 py-6">
-              <Markdown>{description}</Markdown>
-            </div>
-          </div>
+          <Button type="secondary">View Details</Button>
         </div>
-        <div className="">
-          <div className="flex items-center gap-1 sm:gap-2 md:gap-3">
-            <div className="text-xl sm:text-2xl md:text-3xl font-medium">
-              Proposal Outcome
-            </div>
-            <a
-              href={(ipfsLink && getOutcomeLinkFromIpfs(ipfsLink)) || ""}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                id="Layer_1"
-                x="0px"
-                y="0px"
-                className="w-4 sm:w-6 h-4 sm:h-6 fill-black hover:fill-blue"
-                viewBox="0 0 512 512"
-                enableBackground="new 0 0 512 512"
-              >
-                <path d="M459.654,233.373l-90.531,90.5c-49.969,50-131.031,50-181,0c-7.875-7.844-14.031-16.688-19.438-25.813  l42.063-42.063c2-2.016,4.469-3.172,6.828-4.531c2.906,9.938,7.984,19.344,15.797,27.156c24.953,24.969,65.563,24.938,90.5,0  l90.5-90.5c24.969-24.969,24.969-65.563,0-90.516c-24.938-24.953-65.531-24.953-90.5,0l-32.188,32.219  c-26.109-10.172-54.25-12.906-81.641-8.891l68.578-68.578c50-49.984,131.031-49.984,181.031,0  C509.623,102.342,509.623,183.389,459.654,233.373z M220.326,382.186l-32.203,32.219c-24.953,24.938-65.563,24.938-90.516,0  c-24.953-24.969-24.953-65.563,0-90.531l90.516-90.5c24.969-24.969,65.547-24.969,90.5,0c7.797,7.797,12.875,17.203,15.813,27.125  c2.375-1.375,4.813-2.5,6.813-4.5l42.063-42.047c-5.375-9.156-11.563-17.969-19.438-25.828c-49.969-49.984-131.031-49.984-181.016,0  l-90.5,90.5c-49.984,50-49.984,131.031,0,181.031c49.984,49.969,131.031,49.969,181.016,0l68.594-68.594  C274.561,395.092,246.42,392.342,220.326,382.186z" />
-              </svg>
-            </a>
-          </div>
-          <div className="w-full bg-white mt-4 sm:mt-5 md:mt-7 px-3 sm:px-5 md:px-6 py-4 sm:py-7 md:py-9 min-h-24 sm:min-h-32">
-            <div className="flex flex-col gap-3 sm:gap-5 md:gap-8">
-              {outcome &&
-                Object.entries(outcome).map(([key, value]) => (
-                  <OutcomeDetail
-                    key={key}
-                    type={key}
-                    detail={value}
-                    proposalStatus={status}
-                    isXdrInit={isReady}
-                  />
-                ))}
-            </div>
-          </div>
+        <div className="markdown-body w-full px-4 sm:px-6 md:px-8 py-6">
+          <Markdown>{description}</Markdown>
+        </div>
+      </div>
+      <div className="flex flex-col gap-6">
+        <div className="flex items-center gap-3">
+          <p className="text-2xl font-medium text-primary">
+            Proposal Description
+          </p>
+          <a
+            href={(ipfsLink && getOutcomeLinkFromIpfs(ipfsLink)) || ""}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="/icons/link.svg" />
+          </a>
+        </div>
+        <div className="flex flex-col gap-3">
+          {outcome &&
+            Object.entries(outcome).map(([key, value]) => (
+              <OutcomeDetail
+                key={key}
+                type={key}
+                detail={value}
+                isXdrInit={isReady}
+              />
+            ))}
         </div>
       </div>
     </div>
@@ -118,15 +93,10 @@ export default ProposalDetail;
 export const OutcomeDetail: React.FC<{
   type: string;
   detail: { description: string; xdr: string };
-  proposalStatus: ProposalViewStatus | null;
   isXdrInit: boolean;
 }> = ({ type, detail /*, proposalStatus*/, isXdrInit }) => {
   const [content, setContent] = useState<any>(null);
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  const toggleHeight = () => {
-    setIsExpanded(!isExpanded);
-  };
+  const [showXDRModal, setShowXDRModal] = useState(false);
 
   const getContentFromXdr = async (_xdr: string) => {
     try {
@@ -159,73 +129,22 @@ export const OutcomeDetail: React.FC<{
           {detail.description}
         </div>
       </div>
-      <div className="">
-        {content ? (
-          <div className="flex flex-col gap-1 sm:gap-2 md:gap-3">
-            <a
-              href={`${stellarLabViewXdrLink}${modifySlashInXdr(detail.xdr)};;`}
-              target="_blank"
-              rel="noreferrer"
-              className="flex items-center gap-0.5 sm:gap-1 text-black hover:text-blue  group"
-            >
-              <svg
-                role="img"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                id="Stellar--Streamline-Simple-Icons.svg"
-                height="24"
-                width="24"
-                fill="#000000"
-                className="group-hover:fill-blue w-3 h-3 sm:w-4 sm:h-4 md:w-5 md:h-5"
-              >
-                <desc>Stellar Streamline Icon: https://streamlinehq.com</desc>
-                <title>Stellar</title>
-                <path
-                  d="M12.283 1.851A10.154 10.154 0 0 0 1.846 12.002c0 0.259 0.01 0.516 0.03 0.773A1.847 1.847 0 0 1 0.872 14.56L0 15.005v2.074l2.568 -1.309 0.832 -0.424 0.82 -0.417 14.71 -7.496 1.653 -0.842L24 4.85V2.776l-3.387 1.728 -2.89 1.473 -13.955 7.108a8.376 8.376 0 0 1 -0.07 -1.086 8.313 8.313 0 0 1 12.366 -7.247l1.654 -0.843 0.247 -0.126a10.154 10.154 0 0 0 -5.682 -1.932zM24 6.925 5.055 16.571l-1.653 0.844L0 19.15v2.072L3.378 19.5l2.89 -1.473 13.97 -7.117a8.474 8.474 0 0 1 0.07 1.092A8.313 8.313 0 0 1 7.93 19.248l-0.101 0.054 -1.793 0.914a10.154 10.154 0 0 0 16.119 -8.214c0 -0.26 -0.01 -0.522 -0.03 -0.78a1.848 1.848 0 0 1 1.003 -1.785L24 8.992Z"
-                  strokeWidth="1"
-                ></path>
-              </svg>
-              <span className="text-xs sm:text-sm md:text-base">
-                View in Stellar Lab
-              </span>
-            </a>
-            <div
-              className={`transition-all duration-300 ${isExpanded ? "max-h-max" : "max-h-24"} overflow-hidden flex flex-col bg-zinc-100 border border-zinc-400 rounded-xl`}
-            >
-              <div className="w-full py-1 sm:py-2 px-2 sm:px-3 md:px-5">
-                <div
-                  onClick={toggleHeight}
-                  className="w-fit flex items-center gap-2 px-2 py-1 rounded hover:bg-zinc-300 cursor-pointer"
-                >
-                  <span className="text-sm sm:text-base md:text-lg">XDR</span>
-                  <img
-                    src="/icons/arrow-up.svg"
-                    alt="Arrow up"
-                    className={`${isExpanded ? "block" : "hidden"}`}
-                  />
-                  <img
-                    src="/icons/arrow-down.svg"
-                    alt="Arrow down"
-                    className={`${isExpanded ? "hidden" : "block"}`}
-                  />
-                </div>
-              </div>
-              <div className="w-full border-b border-zinc-400"></div>
-              <div className="jsonview-body w-full px-1 sm:px-3 md:px-5 py-2 sm:py-3 md:py-4">
-                <div className={`${isExpanded ? "block" : "hidden"} w-full`}>
-                  <div className="pr-2 md:pr-4 max-h-40 md:max-h-96 overflow-y-auto">
-                    <JsonView src={content} theme="vscode" />
-                  </div>
-                </div>
-              </div>
-            </div>
+      {content && (
+        <Button
+          type="secondary"
+          icon="/icons/eye.svg"
+          onClick={() => setShowXDRModal(true)}
+        >
+          <p className="text-xl text-primary">View XDR</p>
+        </Button>
+      )}
+      {showXDRModal && (
+        <Modal onClose={() => setShowXDRModal(false)}>
+          <div className="max-h-[75vh] overflow-y-auto">
+            <JsonView src={content} theme="vscode" />
           </div>
-        ) : (
-          <div className="text-base md:text-lg px-1 sm:px-2 md:px-4 py-1 md:py-3 bg-zinc-100 border border-zinc-400 rounded-xl">
-            No transaction to execute
-          </div>
-        )}
-      </div>
+        </Modal>
+      )}
     </div>
   );
 };
--- dapp/src/components/page/proposal/ProposalPageTitle.tsx
+++ dapp/src/components/page/proposal/ProposalPageTitle.tsx
@@ -1,29 +1,30 @@
 import { useStore } from "@nanostores/react";
-import React from "react";
-import { useState, useEffect } from "react";
+import Button from "components/utils/Button";
+import React, { useEffect, useState } from "react";
+import type { ProposalView, VoteType } from "types/proposal";
 import { connectedPublicKey } from "utils/store";
-import type { ProposalViewStatus } from "types/proposal";
+import ProposalStatusSection from "./ProposalStatusSection";
+import VoteStatusBar from "./VoteStatusBar";
+import VotersModal from "./VotersModal";
+
 interface Props {
-  id: string;
-  title: string;
+  proposal: ProposalView | null;
   maintainers: string[];
   submitVote: () => void;
   executeProposal: () => void;
-  status: ProposalViewStatus | null;
 }
 
 const ProposalPageTitle: React.FC<Props> = ({
-  id,
-  title,
+  proposal,
   maintainers,
   submitVote,
   executeProposal,
-  status,
 }) => {
-  const [projectId, setProjectId] = useState(id);
   const [isMaintainer, setIsMaintainer] = useState(false);
   const [isConnected, setIsConnected] = useState(false);
   const connectedAddress = useStore(connectedPublicKey);
+  const [voteType, setVoteType] = useState<VoteType>();
+  const [isVotersModalOpen, setIsVotersModalOpen] = useState(false);
 
   const checkIsConnected = () => {
     if (connectedAddress) {
@@ -49,55 +50,83 @@ const ProposalPageTitle: React.FC<Props> = ({
     checkIsConnected();
   }, [connectedAddress]);
 
-  useEffect(() => {
-    if (id) {
-      setProjectId(id);
+  const openVotersModal = (voteType: VoteType) => {
+    if (proposal?.status !== "active") {
+      setVoteType(voteType);
+      setIsVotersModalOpen(true);
+    } else {
+      alert("Cannot show voters while voting is in progress");
     }
-  }, [id]);
+  };
 
   return (
-    <div className="relative flex flex-col items-center md:flex-row justify-between mb-1.5 mt-4 sm:mt-8 md:mt-12 gap-y-4">
-      <div className="grid place-items-center gap-8 md:flex">
-        <span className="w-14 sm:w-16 md:w-20 flex justify-center text-2xl sm:text-3xl md:text-4xl px-1 font-medium bg-lime rounded-md sm:rounded-lg">
-          {projectId}
-        </span>
-        <p className="text-2xl sm:text-3xl lg:text-[34px] font-normal text-center md:text-start">
-          {title}
-        </p>
-      </div>
-      <div className="flex">
-        <div
-          id="vote-proposal-button"
-          className={`${status !== "active" ? "hidden" : "block"}`}
-        >
-          <button
-            disabled={status !== "active"}
-            onClick={() => submitVote()}
-            className={`w-full px-3 md:px-4 py-1 sm:py-1.5 md:py-3 ${!isConnected ? "bg-zinc-600" : "bg-zinc-900"} rounded-lg sm:rounded-xl md:rounded-[14px] justify-center gap-2.5 inline-flex`}
-          >
-            <span className="text-center text-white text-base sm:text-lg md:text-xl font-normal">
-              Vote
-            </span>
-          </button>
+    <>
+      <div className="flex flex-col gap-[30px]">
+        <div className="flex flex-col gap-[18px]">
+          <div className="flex items-center gap-3">
+            <p className="text-base text-[#695A77]">Created by</p>
+            <div className="flex items-center gap-2">
+              {isMaintainer && (
+                <div className="p-[2px_10px_4px_10px] bg-[#FFEFA8] rounded-[36px]">
+                  <p className="text-base font-semibold text-[#2D0F51]">
+                    maintainer
+                  </p>
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="flex flex-col gap-3">
+            <p className="text-2xl font-medium text-primary">
+              {proposal?.id} {proposal?.title}
+            </p>
+            <VoteStatusBar
+              approve={proposal?.voteStatus?.approve?.score || 0}
+              reject={proposal?.voteStatus?.reject?.score || 0}
+              abstain={proposal?.voteStatus?.abstain?.score || 0}
+              onClick={(voteType) => openVotersModal(voteType)}
+            />
+          </div>
         </div>
-        <div
-          id="vote-proposal-button"
-          className={`${status === "voted" && isMaintainer ? "block" : "hidden"}`}
-        >
-          <button
-            disabled={status !== "voted" || !isMaintainer}
-            onClick={() => {
-              executeProposal();
-            }}
-            className={`w-full px-3 md:px-4 py-1 sm:py-1.5 md:py-3 ${status !== "voted" ? "bg-zinc-600" : "bg-zinc-900"} rounded-lg sm:rounded-xl md:rounded-[14px] justify-center gap-2.5 inline-flex`}
-          >
-            <span className="text-center text-white text-base sm:text-lg md:text-xl font-normal">
-              Execute
-            </span>
-          </button>
+        <div className="flex flex-col gap-6">
+          <div className="flex justify-between">
+            <ProposalStatusSection
+              status={proposal?.status || null}
+              endDate={proposal?.endDate || null}
+            />
+            <Button icon="/icons/vote.svg">Vote</Button>
+            {status == "active" && (
+              <button
+                onClick={() => submitVote()}
+                className={`w-full px-3 md:px-4 py-1 sm:py-1.5 md:py-3 ${!isConnected ? "bg-zinc-600" : "bg-zinc-900"} rounded-lg sm:rounded-xl md:rounded-[14px] justify-center gap-2.5 inline-flex`}
+              >
+                <span className="text-center text-white text-base sm:text-lg md:text-xl font-normal">
+                  Vote
+                </span>
+              </button>
+            )}
+            {status == "voted" && isMaintainer && (
+              <button
+                onClick={() => executeProposal()}
+                className={`w-full px-3 md:px-4 py-1 sm:py-1.5 md:py-3 ${status !== "voted" ? "bg-zinc-600" : "bg-zinc-900"} rounded-lg sm:rounded-xl md:rounded-[14px] justify-center gap-2.5 inline-flex`}
+              >
+                <span className="text-center text-white text-base sm:text-lg md:text-xl font-normal">
+                  Execute
+                </span>
+              </button>
+            )}
+          </div>
+          <div className="h-[1px] bg-[#EEEEEE]" />
         </div>
       </div>
-    </div>
+      {isVotersModalOpen && proposal?.voteStatus && (
+        <VotersModal
+          NQGScore={proposal?.nqg || 0}
+          voteData={(voteType && proposal?.voteStatus?.[voteType]) || null}
+          projectMaintainers={maintainers}
+          onClose={() => setIsVotersModalOpen(false)}
+        />
+      )}
+    </>
   );
 };
 
