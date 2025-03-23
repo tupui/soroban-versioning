5e363caab40238282134c8241ca7de682798d702
clintjeff2 <clintjeff2@gmail.com>
2025-03-23 15:29:25 +0100

    Removed everything that had to do with @astrojs/tailwind as it is deprecated for version 4.x of tailwind css

--- dapp/astro.config.mjs
+++ dapp/astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+// import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react()],
   adapter: netlify(),
 });
