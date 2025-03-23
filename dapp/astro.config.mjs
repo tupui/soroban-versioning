3214d5678c68378b54edf93cfcbd4b5f689bf8f7
clintjeff2 <clintjeff2@gmail.com>
2025-03-23 18:20:59 +0100

    Removed unnecessary comment of @astrojs/tailwind

--- dapp/astro.config.mjs
+++ dapp/astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-// import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
