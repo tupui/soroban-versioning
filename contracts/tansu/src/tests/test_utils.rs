1e2b14f96af7bcaf36d592281ff94ebf11a64070
Tupui <23188539+tupui@users.noreply.github.com>
2025-09-29 13:02:12 +0200

    Add XLM contract and Reflector snapshots

--- contracts/tansu/src/tests/test_utils.rs
+++ contracts/tansu/src/tests/test_utils.rs
@@ -1,6 +1,6 @@
 use crate::{Tansu, TansuClient, types};
-use soroban_sdk::testutils::Address as _;
-use soroban_sdk::{Address, Bytes, Env, Executable, String, Vec, token, vec};
+use soroban_sdk::testutils::{Address as _, Ledger};
+use soroban_sdk::{Address, Bytes, Env, Executable, String, token, vec};
 
 pub struct TestSetup {
     pub env: Env,
@@ -16,6 +16,8 @@ pub struct TestSetup {
 pub fn create_env() -> Env {
     let env = Env::from_ledger_snapshot_file("../../network_snapshots/sorobandomains.json");
     env.mock_all_auths();
+    env.ledger().set_protocol_version(23);
+    env.ledger().set_timestamp(1759139101); // keep in sync with snapshot last_timestamp (remove 3 zeros)
     env
 }
 
@@ -24,10 +26,6 @@ pub fn create_test_data() -> TestSetup {
 
     let contract_strkey = "CATRNPHYKNXAPNLHEYH55REB6YSAJLGCPA4YM6L3WUKSZOPI77M2UMKI";
     let domain_id = Address::from_str(&env, contract_strkey);
-
-    let issuer = Address::generate(&env);
-    let sac = env.register_stellar_asset_contract_v2(issuer.clone());
-    let token_stellar = token::StellarAssetClient::new(&env, &sac.address());
 
     let contract_admin = Address::generate(&env);
     let contract_id = env.register(Tansu, (&contract_admin,));
@@ -46,14 +44,29 @@ pub fn create_test_data() -> TestSetup {
     };
     contract.set_domain_contract(&contract_admin, &new_domain);
 
+    let issuer = Address::generate(&env);
+    let sac = env.register_stellar_asset_contract_v2(issuer.clone());
+
     let new_collateral = types::Contract {
         address: sac.address(),
         wasm_hash: None,
     };
     contract.set_collateral_contract(&contract_admin, &new_collateral);
 
+    let token_stellar = token::StellarAssetClient::new(&env, &sac.address());
     let grogu = Address::generate(&env);
     let mando = Address::generate(&env);
+    let genesis_amount: i128 = i128::MAX;
+    token_stellar.mint(&grogu, &genesis_amount);
+    token_stellar.mint(&mando, &genesis_amount);
+
+    // we need real XLM, using some data from the snapshot
+    let contract_strkey = "CAS3J7GYLGXMF6TDJBBYYSE3HQ6BBSMLNUQ34T6TZMYMW2EVH34XOWMA";
+    let xlm_id = Address::from_str(&env, contract_strkey);
+    let token_stellar_xlm = token::StellarAssetClient::new(&env, &xlm_id);
+    let random_account_strkey = "CAC3AUH3A3ZCABCOPDCXVJXSIL57WHAPGUPTKAOMSNFLAVE7CPPGNGZB";
+    let random_account_id = Address::from_str(&env, random_account_strkey);
+    token_stellar_xlm.transfer(&random_account_id, &grogu, &1583624677996i128);
 
     TestSetup {
         env,
@@ -72,10 +85,6 @@ pub fn init_contract(setup: &TestSetup) -> Bytes {
     let url = String::from_str(&setup.env, "github.com/tansu");
     let ipfs = String::from_str(&setup.env, "2ef4f49fdd8fa9dc463f1f06a094c26b88710990");
     let maintainers = vec![&setup.env, setup.grogu.clone(), setup.mando.clone()];
-
-    let genesis_amount: i128 = 1_000_000_000 * 10_000_000;
-    setup.token_stellar.mint(&setup.grogu, &genesis_amount);
-    setup.token_stellar.mint(&setup.mando, &genesis_amount);
 
     setup
         .contract
