f75d2717782f61b23cf2e83142a505b6e75915a3
masch <the.masch@gmail.com>
2025-12-24 15:19:56 -0300

    feat: Add `add_projects_to_pagination` to migrate existing projects to a single pagination page.

--- contracts/tansu/src/contract_migration.rs
+++ contracts/tansu/src/contract_migration.rs
@@ -2,26 +2,29 @@ use soroban_sdk::{Address, Bytes, Env, String, Vec, contractimpl};
 
 use crate::{MigrationTrait, Tansu, TansuArgs, TansuClient, TansuTrait, types};
 
-const MAX_PROJECTS_PER_PAGE: u32 = 10;
+const LAST_PAGE: u32 = 0;
 
 #[contractimpl]
 impl MigrationTrait for Tansu {
+    /// Add projects to pagination. This is used to migrate projects when the project was created before the pagination was implemented.
+    ///
+    /// # Arguments
+    /// * `env` - The environment object
+    /// * `admin` - The admin address
+    /// * `names` - The names of the projects to add
+    ///
+    /// # Returns
+    /// * `()`
     fn add_projects_to_pagination(env: Env, admin: Address, names: Vec<String>) {
         Tansu::require_not_paused(env.clone());
         crate::contract_tansu::auth_admin(&env, &admin);
 
-        let mut total_projects = env
+        let mut total_projects_paginated = 0;
+
+        let mut project_keys_page: Vec<Bytes> = env
             .storage()
             .persistent()
-            .get(&types::ProjectKey::TotalProjects)
-            .unwrap_or(0u32);
-
-        let page = total_projects / MAX_PROJECTS_PER_PAGE;
-
-        let mut project_keys: Vec<Bytes> = env
-            .storage()
-            .persistent()
-            .get(&types::ProjectKey::ProjectKeys(page))
+            .get(&types::ProjectKey::ProjectKeys(LAST_PAGE))
             .unwrap_or(Vec::new(&env));
 
         for name in names {
@@ -35,21 +38,22 @@ impl MigrationTrait for Tansu {
 
             // Only migrate if the project exists but might not be in the pagination list
             if env.storage().persistent().has(&key_) {
-                if !project_keys.contains(key.clone()) {
-                    project_keys.push_back(key.clone());
-                    total_projects += 1;
+                if !project_keys_page.contains(key.clone()) {
+                    project_keys_page.push_back(key.clone());
+                    total_projects_paginated += 1;
                 }
             }
         }
 
-        if total_projects > 0 {
-            env.storage()
-                .persistent()
-                .set(&types::ProjectKey::ProjectKeys(page), &project_keys);
+        if total_projects_paginated > 0 {
+            env.storage().persistent().set(
+                &types::ProjectKey::ProjectKeys(LAST_PAGE),
+                &project_keys_page,
+            );
 
             env.storage()
                 .persistent()
-                .set(&types::ProjectKey::TotalProjects, &total_projects);
+                .set(&types::ProjectKey::TotalProjects, &total_projects_paginated);
         }
     }
 }
