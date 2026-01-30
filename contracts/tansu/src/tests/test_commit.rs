e8e86083094af1bb2af1f9006a7bb4e051f4f748
SOMIE <somie@example.com>
2026-01-30 13:30:39 +0100

    fix

--- contracts/tansu/src/tests/test_commit.rs
+++ contracts/tansu/src/tests/test_commit.rs
@@ -23,9 +23,14 @@ fn commit_events() {
     let hash_commit = String::from_str(&setup.env, "6663520bd9e6ede248fef8157b2af0b6b6b41046");
     setup.contract.commit(&setup.mando, &id, &hash_commit);
 
-    let all_events = setup.env.events().all();
+  let contract_events = setup
+        .env
+        .events()
+        .all()
+        .filter_by_contract(&setup.contract_id);
+
     assert_eq!(
-        all_events,
+        contract_events,
         vec![
             &setup.env,
             (
