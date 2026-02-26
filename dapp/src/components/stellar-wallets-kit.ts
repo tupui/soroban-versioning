f139207ecf7d1cc49fa61679c69cd9c526e88749
devfoma <oluebube.madu@yahoo.com>
2026-02-26 04:00:50 -0800

    update wallet kit to latest

--- dapp/src/components/stellar-wallets-kit.ts
+++ dapp/src/components/stellar-wallets-kit.ts
@@ -1,10 +1,8 @@
-/**
- * Stellar Wallets Kit Initialization
- * 
- * Re-exports from the wrapper module which handles the dual implementation.
- * Uses feature flag (USE_NEW_WALLET_KIT env var) to switch between:
- * - Legacy: @creit.tech/stellar-wallets-kit@^1.9.5 (npm)
- * - New: @creit.tech/stellar-wallets-kit (JSR)
- */
+import { StellarWalletsKit } from "@creit-tech/stellar-wallets-kit/sdk";
+import { defaultModules } from "@creit-tech/stellar-wallets-kit/modules/utils";
 
-export { getWalletKit as getKit} from "./stellar-wallets-kit-wrapper";
+StellarWalletsKit.init({
+  modules: defaultModules(),
+});
+
+export { StellarWalletsKit };
